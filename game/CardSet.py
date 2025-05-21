b2fa8d8a1dbe13278c6ee2ee744e8f5ac63184ab
ubiny227 <ubiny227@gmail.com>
2025-05-21 15:23:01 +0900

    Card shuffle 메서드 추가

--- game/CardSet.py
+++ game/CardSet.py
@@ -1,5 +1,5 @@
+import random
 from game.Card import Card # Card 파일에서 Card 클래스 불러옴
-
 class CardSet: # 카드셋 클래스의 도입부
     def __init__(self): # 클래스 초기화 메서드
         self.resize() # 카드 개수를 세트
@@ -13,5 +13,4 @@ class CardSet: # 카드셋 클래스의 도입부
         self.cards = [Card(i) for i in range(0, self.size, 2)] # 클래스의 cards필드에 카드들을 사이즈에 맞게 세트
 
     def shuffle(self): # 카드를 섞는 메서드
-        # TODO: 김민규 구현
-        raise NotImplementedError
+        random.shuffle(self.cards)
